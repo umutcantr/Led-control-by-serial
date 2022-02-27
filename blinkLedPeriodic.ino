6be602a05c7a1a843f76d8a8adccc3de890928ae
Umutcan Genç <umut285830@gmail.com>
2022-02-27 15:50:28 +0300

    readability improved and wait function added

--- blinkLedPeriodic.ino
+++ blinkLedPeriodic.ino
@@ -4,76 +4,86 @@
 /*** Function Prototypes ***/
 void blinkLed(void);
 void readSerial(void);
+bool wait(int period, unsigned long *currentTime, unsigned long *previousTimeLed);
 
 /*** External variables ***/
- unsigned long previousTimeLed = millis();   
- unsigned long previousTimeLed1 = millis(); 
- long timeIntervalLed = 1000;
+
+ int ledOnPeriod = 1000;                    // led on period default 1 sec
+ int ledOffPeriod = 1000;                   // led off period default 1 sec
+
  
- int ledState = LOW;
- String incomingData;
- char START[6] = "start";
- char STOP[5] = "stop";
- char LED[4] = "led";
- char ON[3] = "on";
- char OFF[4] = "off";
- char delimiter = '=';
- int echoState = 1;
-
- char *ptr;
-
- int ledOnPeriod = 1000;      // led on period default 1 sec
- int ledOffPeriod = 1000;      // led off period default 1 sec
-
 void setup() {
   pinMode(led, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  blinkLed();      // task for blink led 1 sec period
-  readSerial();    // read incoming data and echo data for "start" command
+  blinkLed();                               // task for blink led 1 sec period
+  readSerial();                             // task for read incoming data and echo data for "start" command
 }
 
+// Task 1
+void blinkLed(void){
+   static unsigned long previousTimeLed = millis();      // set initial uc time   
+   unsigned long currentTime;
+   static int ledState = LOW;                            // led state default to LOW
 
-void blinkLed(){
-  unsigned long currentTime = millis();
-  if((currentTime - previousTimeLed > ledOffPeriod) && ledState == LOW) {    // waits for timeIntervalLed value without blocking
-    previousTimeLed = currentTime;
+  if(wait(ledOffPeriod, &currentTime, &previousTimeLed) && ledState == LOW) {    // check elapsed time and led state 
+    previousTimeLed = currentTime;               // update previous time led value
     ledState = HIGH;
   }
   
-  if((currentTime - previousTimeLed > ledOnPeriod) && ledState == HIGH) {    // waits for timeIntervalLed value without blocking
-    previousTimeLed = currentTime;
+  if(wait(ledOnPeriod, &currentTime, &previousTimeLed) && ledState == HIGH) {    // check elapsed time and led state 
+    previousTimeLed = currentTime;               // update previous time led value
     ledState = LOW;
   }
-  digitalWrite(led, ledState);
+  digitalWrite(led, ledState);                   // blink led
 }
 
+// Task 2
 void readSerial(void){
+ char buf[20];                              // incoming uart data
+ char START[6] = "start";                   
+ char STOP[5] = "stop";
+ char LED[4] = "led";
+ char ON[3] = "on";
+ char OFF[4] = "off";
+ char delimiter = '=';                      // delimiter of incoming data
+ char *delimiterPtr;                        // points to delimiter in incoming data 
+ int echoState = 1;                         // defualt to echo incoming data 
+ 
   if(Serial.available()){
-    incomingData = Serial.readString();
-    
-    if(!strncmp(incomingData.c_str(), START, 5)){   // compare incomingData string  and "start" string first 5 character.
-      echoState = 1;             // echo incomingData string
+    Serial.readBytesUntil('\n', buf, sizeof(buf)/sizeof(buf[0]));
+    //Serial.println(buf);
+    if(strstr(buf, START)){   // compare incomingData string  and "start" string first 5 character.
+      echoState = 1;                                // echo incomingData string
     }
-    else if(!strncmp(incomingData.c_str(), STOP, 4)){   // compare incomingData string  and "stop" string first 4 character.
+    else if(strstr(buf, STOP)){   // compare incomingData string  and "stop" string first 4 character.
       ledOnPeriod = 1000;                          // set led on period 
       ledOffPeriod = 1000;                         // set led off period 
       echoState = 0;
     }
-    else if(strstr(incomingData.c_str(), LED)){   // compare incomingData string  and "stop" string first 4 character.
-      if(strstr(incomingData.c_str(), ON)){
-        ptr = strchr(incomingData.c_str(), delimiter);
-        ledOnPeriod = atoi(ptr+1);
+    else if(strstr(buf, LED)){   // compare incomingData string  and "stop" string first 4 character.
+      if(strstr(buf, ON)){
+        delimiterPtr = strchr(buf, delimiter);
+        ledOnPeriod = atoi(delimiterPtr+1);
       }
-      else if(strstr(incomingData.c_str(), OFF)){
-        ptr = strchr(incomingData.c_str(), delimiter);
-        ledOffPeriod = atoi(ptr+1);
+      else if(strstr(buf, OFF)){
+        delimiterPtr = strchr(buf, delimiter);
+        ledOffPeriod = atoi(delimiterPtr+1);
       }
     }
     if(echoState){
-      Serial.print(incomingData);
+      Serial.println(buf);
     }
   }
+  memset(buf, 0, sizeof(buf)/sizeof(buf[0]));
+}
+
+// if elapsed time bigger than period value return true, otherwise false
+// takes wait period, previousTime and currentTime arguments
+bool wait(int period, unsigned long *currentTime, unsigned long *previousTime){
+  *currentTime = millis();
+  if(*currentTime - *previousTime > period) return true;
+  else                                          return false;
 }
