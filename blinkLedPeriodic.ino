3fab75ecf103ff065375591c86a03397930ae8d4
Umutcan Genç <umut285830@gmail.com>
2022-02-27 13:29:01 +0300

    ledon/ledoff/start/stop tasks added

--- blinkLedPeriodic.ino
+++ blinkLedPeriodic.ino
@@ -7,11 +7,23 @@ void readSerial(void);
 
 /*** External variables ***/
  unsigned long previousTimeLed = millis();   
+ unsigned long previousTimeLed1 = millis(); 
  long timeIntervalLed = 1000;
  
- int ledState;
+ int ledState = LOW;
  String incomingData;
  char START[6] = "start";
+ char STOP[5] = "stop";
+ char LED[4] = "led";
+ char ON[3] = "on";
+ char OFF[4] = "off";
+ char delimiter = '=';
+ int echoState = 1;
+
+ char *ptr;
+
+ int ledOnPeriod = 1000;      // led on period default 1 sec
+ int ledOffPeriod = 1000;      // led off period default 1 sec
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -26,14 +38,16 @@ void loop() {
 
 void blinkLed(){
   unsigned long currentTime = millis();
-  if(currentTime - previousTimeLed > timeIntervalLed) {    // waits for timeIntervalLed value without blocking
+  if((currentTime - previousTimeLed > ledOffPeriod) && ledState == LOW) {    // waits for timeIntervalLed value without blocking
     previousTimeLed = currentTime;
-    
-    if (ledState == HIGH) ledState = LOW;
-    else                  ledState = HIGH; 
-    
-    digitalWrite(led, ledState);
-    }
+    ledState = HIGH;
+  }
+  
+  if((currentTime - previousTimeLed > ledOnPeriod) && ledState == HIGH) {    // waits for timeIntervalLed value without blocking
+    previousTimeLed = currentTime;
+    ledState = LOW;
+  }
+  digitalWrite(led, ledState);
 }
 
 void readSerial(void){
@@ -41,7 +55,25 @@ void readSerial(void){
     incomingData = Serial.readString();
     
     if(!strncmp(incomingData.c_str(), START, 5)){   // compare incomingData string  and "start" string first 5 character.
-      Serial.println(incomingData);                 // echo incomingData string
+      echoState = 1;             // echo incomingData string
+    }
+    else if(!strncmp(incomingData.c_str(), STOP, 4)){   // compare incomingData string  and "stop" string first 4 character.
+      ledOnPeriod = 1000;                          // set led on period 
+      ledOffPeriod = 1000;                         // set led off period 
+      echoState = 0;
+    }
+    else if(strstr(incomingData.c_str(), LED)){   // compare incomingData string  and "stop" string first 4 character.
+      if(strstr(incomingData.c_str(), ON)){
+        ptr = strchr(incomingData.c_str(), delimiter);
+        ledOnPeriod = atoi(ptr+1);
+      }
+      else if(strstr(incomingData.c_str(), OFF)){
+        ptr = strchr(incomingData.c_str(), delimiter);
+        ledOffPeriod = atoi(ptr+1);
+      }
+    }
+    if(echoState){
+      Serial.print(incomingData);
     }
   }
 }
